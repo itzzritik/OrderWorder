b4c8ff952725464fa69926ba1bddd3e94b7396f5
Ritik Srivastava <ritik.space@gmail.com>
2023-11-19 04:55:55 +0530

    🐛 Disable lint check for next builds

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+        ignoreDuringBuilds: true,
+    },
 
 	webpack(config) {
 		config.module.rules.push({
