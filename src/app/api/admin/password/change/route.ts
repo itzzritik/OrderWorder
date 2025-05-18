f189d3424e9559f414dc242d2d7aaa03f118df9d
Ritik Srivastava <ritik.space@gmail.com>
2025-05-18 06:48:37 +0530

    ✨ add console timers

--- src/app/api/admin/password/change/route.ts
+++ src/app/api/admin/password/change/route.ts
@@ -9,21 +9,33 @@ import { verifyPassword } from '#utils/helper/passwordHelper';
 
 export async function POST (req: Request) {
 	try {
+		console.time('connectDB');
 		await connectDB();
+		console.timeEnd('connectDB');
+		console.time('getSession');
 		const session = await getServerSession(authOptions);
+		console.timeEnd('getSession');
 		const { password, newPassword } = await req.json();
 
 		if (!session) throw { status: 401, message: 'Authentication Required' };
 		if (!password) throw { status: 400, message: 'Password Required' };
 		if (!newPassword) throw { status: 400, message: 'New Password Required' };
 
+		console.time('findAccount');
 		const account = await Accounts.findOne<TAccount>({ username: session?.username });
+		console.timeEnd('findAccount');
 
 		if (!account) throw { status: 500, message: 'Something went wrong' };
 
-		if (verifyPassword(password, account?.password)) {
+		console.time('verifyPassword');
+		const valid = verifyPassword(password, account.password);
+		console.timeEnd('verifyPassword');
+
+		if (valid) {
 			account.password = newPassword;
+			console.time('saveAccount');
 			await account.save();
+			console.timeEnd('saveAccount');
 			return NextResponse.json({ status: 200, message: 'Password successfully changed' });
 		}
 
