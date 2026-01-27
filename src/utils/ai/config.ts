6dcdca19ae33a3d7ff3debf3a15f321ec60b9c19
Ritik Srivastava <ritik.space@gmail.com>
2026-01-27 06:07:09 +0530

    Improve model configs

--- src/utils/ai/config.ts
+++ src/utils/ai/config.ts
@@ -2,16 +2,10 @@ import { createCerebras } from "@ai-sdk/cerebras";
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { createGroq } from "@ai-sdk/groq";
 
-const Providers = {
-	groq: createGroq({ apiKey: process.env.GROQ_KEY }),
-	google: createGoogleGenerativeAI({ apiKey: process.env.GEMINI_KEY }),
-	cerebras: createCerebras({ apiKey: process.env.CEREBRAS_KEY }),
-};
-
 const models = {
-	gemini: Providers.google("gemma-3-27b-it"),
-	cerebras: Providers.cerebras("llama-3.3-70b"),
-	groq: Providers.groq("meta-llama/llama-4-scout-17b-16e-instruct"),
+	groq: createGroq({ apiKey: process.env.GROQ_KEY })("meta-llama/llama-4-scout-17b-16e-instruct"),
+	cerebras: createCerebras({ apiKey: process.env.CEREBRAS_KEY })("llama-3.3-70b"),
+	google: createGoogleGenerativeAI({ apiKey: process.env.GEMINI_KEY })("gemma-3-27b-it"),
 };
 
 export const getModel = (provider: keyof typeof models) => models[provider];
